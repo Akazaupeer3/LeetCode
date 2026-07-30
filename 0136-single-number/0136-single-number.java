5b84807e58a4d12c786705a8afa04aeb010dd77d
Abdul Rafay <L1F24BSSE0254@UCP.EDU.PK>
2026-07-30 14:58:59 +0500

    Time: 1 ms (99.96%), Space: 47.1 MB (27.95%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int unique = 0;
+    
+         for (int num : nums){
+            unique = unique ^ num;
+        }
+        return unique;
+    }
+}
